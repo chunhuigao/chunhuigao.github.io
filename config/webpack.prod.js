26372edf5a2e08cd783a66d36ff60828ea0ebbb1
北斗 <1912128546@qq.com>
2022-03-26 08:53:40 +0800

    🐳 chore:增加Babel依赖

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,10 +1,9 @@
-const { merge } = require("webpack-merge")
-const common = require("./webpack.common.js")
-const { CleanWebpackPlugin } = require("clean-webpack-plugin")
-const BundleAnalyzerPlugin =
-  require("webpack-bundle-analyzer").BundleAnalyzerPlugin
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.js')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = merge(common, {
-  mode: "production",
-  plugins: [new CleanWebpackPlugin(), new BundleAnalyzerPlugin()],
+  mode: 'production',
+  plugins: [new CleanWebpackPlugin()],
 })
